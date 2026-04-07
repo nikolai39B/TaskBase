ec050f89c4c7c91cc50d562d53124684ec06ea5e
Will Hauber <nikolai39b@gmail.com>
2026-04-07 18:15:46 -0400

    checkpoint

--- src/metadata/statusProperty.ts
+++ src/metadata/statusProperty.ts
@@ -1,22 +1,22 @@
 import { ComplexPropertyValue } from './metadataTypes';
 
-export class Status {
-    constructor
-}
-
-export StatusPropertyValue: ComplexPropertyValue<string, string> = {
-    name: string;
-    display: string;
-    isResolved?: boolean;
-}
-
-export const DateTimePropertyValue: ComplexPropertyValue<Date, string> = {
-    serialize:    (date) => date.toISOString(), // "2024-03-15T10:30:00.000Z"
-    deserialize:  (raw) => {
-        const date = new Date(raw);
-        if (isNaN(date.getTime())) {
-            return undefined;
-        }
-        return date;// as DateTime;
-    },
-};
+//export class Status {
+//    constructor
+//}
+//
+//export StatusPropertyValue: ComplexPropertyValue<string, string> = {
+//    name: string;
+//    display: string;
+//    isResolved?: boolean;
+//}
+//
+//export const DateTimePropertyValue: ComplexPropertyValue<Date, string> = {
+//    serialize:    (date) => date.toISOString(), // "2024-03-15T10:30:00.000Z"
+//    deserialize:  (raw) => {
+//        const date = new Date(raw);
+//        if (isNaN(date.getTime())) {
+//            return undefined;
+//        }
+//        return date;// as DateTime;
+//    },
+//};
